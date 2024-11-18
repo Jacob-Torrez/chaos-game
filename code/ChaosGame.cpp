7238f0e89bdb0209847366f6081cb12bf8dff580
Aegisosh <jacob.torrez2005@gmail.com>
2024-11-18 11:23:33 -0800

    moving to vs

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -9,8 +9,7 @@
 using namespace sf;
 using namespace std;
 
-int main()
-{
+int main(){
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
@@ -19,43 +18,39 @@ int main()
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
 
-	while (window.isOpen())
-	{
+	while (window.isOpen()){
 		/*
 		****************************************
 		Handle the players input
 		****************************************
 		*/
 		Event event;
-		while (window.pollEvent(event))
-		{
-		    if (event.type == Event::Closed)
-		    {
+		while (window.pollEvent(event)){
+		    if (event.type == Event::Closed){
 					// Quit the game when the window is closed
 					window.close();
 		    }
-		    if (event.type == sf::Event::MouseButtonPressed)
+		    
+			if (event.type == sf::Event::MouseButtonPressed)
 		    {
-			if (event.mouseButton.button == sf::Mouse::Left)
-			{
-			    std::cout << "the left button was pressed" << std::endl;
-			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-	
-			    if(vertices.size() < 3)
-			    {
-				vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-			    }
-			    else if(points.size() == 0)
-			    {
-				///fourth click
-				///push back to points vector
-			    }
-			}
+				if (event.mouseButton.button == sf::Mouse::Left){
+					std::cout << "the left button was pressed" << std::endl;
+					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+		
+					if(vertices.size() < 3){
+						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+					else if(points.size() == 0){
+						///fourth click
+						///push back to points vector
+						points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+				}
 		    }
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{
+		
+		if (Keyboard::isKeyPressed(Keyboard::Escape)){
 			window.close();
 		}
 		/*
@@ -64,8 +59,7 @@ int main()
 		****************************************
 		*/
 	
-		if(points.size() > 0)
-		{
+		if(points.size() > 0){
 		    ///generate more point(s)
 		    ///select random vertex
 		    ///calculate midpoint between random vertex and the last point in the vector
@@ -78,8 +72,7 @@ int main()
 		****************************************
 		*/
 		window.clear();
-		for(int i = 0; i < vertices.size(); i++)
-		{
+		for(int i = 0; i < vertices.size(); i++){
 		    RectangleShape rect(Vector2f(10,10));
 		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
 		    rect.setFillColor(Color::Blue);
